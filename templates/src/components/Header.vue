cb6acd376875966f32a5c24bb33cab9f6eea1ab0
Constaninely <kvlod2001@gmail.com>
2019-10-20 00:25:56 +0300

    полностью главная

--- templates/src/components/Header.vue
+++ templates/src/components/Header.vue
@@ -1,10 +1,9 @@
 <template>
     <header id="header-root">
-        <b-nav class="d-flex justify-content-around">
-            <b-nav-item href="#mainDiv">Главная</b-nav-item>
-            <b-nav-item href="#yakor-rules">Правила</b-nav-item>
-            <b-nav-item href="#yakor-nominations">Номинации </b-nav-item>
-            <b-nav-item href="#yakor-about">О нас</b-nav-item>
+        <b-nav class="d-flex justify-content-center" >
+                <b-nav-item :active="currentActive==link.id?true:false" v-for="link in links" :key="link.id" :href="link.url" class="flex-fill text-center align-self-start" @click="changeActive(link.id)" >
+                    {{link.text}}
+                </b-nav-item>
         </b-nav>       
   
     </header>
@@ -13,6 +12,22 @@
 
 <script>
 export default {
-    name: 'header'
+    name: 'header',
+    data() {
+        return {
+            links:[
+                {id:0, url:'#mainDiv', text:'Главная'},
+                {id:1, url:'#yakor-rules', text:'Правила'},
+                {id:2, url:'#yakor-nominations', text:'Номинации'},
+                {id:3, url:'#yakor-about', text:'О нас'}
+            ],
+            currentActive:0
+        }
+    },
+    methods: {
+        changeActive(id){
+            this.currentActive = id
+        }
+    }
 }
 </script>
