32d21c5a220c1e77a05ea080a96577734082d669
Roman Naumtsov <TheFlexMAX@mail.ru>
2019-10-20 02:54:30 +0300

    Добавлена первая форма, маршрутизация, логика

--- Hackaton/urls.py
+++ Hackaton/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+# Временный view
+from odessa_courtyards.views import MemberRequestCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('odessa_courtyards.urls')),
     path('odessa_courtyards/', include('odessa_courtyards.urls')),
+
+    # временная маршрутизация
+    path('form/', MemberRequestCreate.as_view())
 ]
--- odessa_courtyards/views.py
+++ odessa_courtyards/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import View
+
+from .forms import (
+    MemberRequestForm
+)
 
 from .models import MemberRequest, FormBefore, ProblemArea
                     #FormAfter, ReformedAreas
@@ -10,10 +17,34 @@ class MemberRequestViewSet(viewsets.ModelViewSet):
     serializer_class = MemberRequestSerializer
     queryset = MemberRequest.objects.all()
 
+
 class FormBeforeViewSet(viewsets.ModelViewSet):
     serializer_class = FormBeforeSerializer
     queryset = FormBefore.objects.all()
 
+
 class ProblemAreaViewSet(viewsets.ModelViewSet):
     serializer_class = ProblemAreaSerializer
     queryset = ProblemArea.objects.all()
+
+
+'''
+    Классы обработки запросов без view
+'''
+
+# Логика обработки формы создания нового студента из формы
+class MemberRequestCreate(View):
+    def get(self, request):
+        form = MemberRequestForm()
+        return render(request, 'form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = MemberRequestForm(request.POST)
+
+        print(f"bound_form.is_valid(): {bound_form.is_valid()}")
+        print(f"bound_form: {bound_form}")
+        if bound_form.is_valid():
+            new_member_request = bound_form.save()
+            return redirect('../')
+        return render(request, 'form.html', context={'form': bound_form})
+
