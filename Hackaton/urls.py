164d663279c3877df14badd9686bbd7de8b3a61c
Roman Naumtsov <TheFlexMAX@mail.ru>
2019-10-20 11:19:07 +0300

    Добавлены интерфейсы работы с апи

--- Hackaton/urls.py
+++ Hackaton/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 # Временный view
-from odessa_courtyards.views import MemberRequestCreate
+from odessa_courtyards.views import MemberRequestCreate, FormBeforeCreate, FormAfterCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('odessa_courtyards.urls')),
-    path('odessa_courtyards/', include('odessa_courtyards.urls')),
-
+    path('api/', include('odessa_courtyards.urls')),
     # временная маршрутизация
-    path('form/', MemberRequestCreate.as_view())
+    path('member-request', MemberRequestCreate.as_view()),
+    path('form-before', FormBeforeCreate.as_view()),
+    path('FormAfterCreate', FormAfterCreate.as_view())
 ]
